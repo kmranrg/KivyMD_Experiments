27f9b330f9df9c4e5ee3ed95501d70d54a7453c1
kmranrg <kmranrg@gmail.com>
2020-09-09 22:02:24 +0530

    added labels and buttons

---
+++ 2_Labels/main.py
@@ -0,0 +1,12 @@
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel, MDIcon
+
+class DemoApp(MDApp):
+    def build(self):
+        label = MDLabel(text="Hello World", halign='center', theme_text_color='Custom',
+                        text_color=(236/255.0, 98/255.0, 81/255.0, 1), font_style="H2")
+
+        icon_label = MDIcon(icon="language-python", halign='center')
+        return icon_label
+
+DemoApp().run()
