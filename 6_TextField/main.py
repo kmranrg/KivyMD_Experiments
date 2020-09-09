a4205ca48fd7748d31ad2c171cd113b8ed9dff86
kmranrg <kmranrg@gmail.com>
2020-09-09 22:47:29 +0530

    added text fiekd

---
+++ 6_TextField/main.py
@@ -0,0 +1,30 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivy.lang import Builder
+
+username_helper = """
+MDTextField:
+    hint_text: "Enter username"
+    helper_text: "or click on Forgot Username"
+    helper_text_mode: "on_focus"
+    icon_right: "android"
+    icon_right_color: app.theme_cls.primary_color
+    pos_hint: {'center_x': 0.5, 'center_y': 0.5}
+    size_hint_x: None
+    width: 300
+"""
+
+
+class DemoApp(MDApp):
+    def build(self):
+        screen = Screen()
+        self.theme_cls.primary_palette = "Green"
+        # username = MDTextField(text="Enter username:", pos_hint={'center_x': 0.5, 'center_y': 0.5},
+        #                        size_hint_x = None, width=300)
+
+        username = Builder.load_string(username_helper)
+        screen.add_widget(username)
+        return screen
+
+
+DemoApp().run()
