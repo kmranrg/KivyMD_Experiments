ffe1d4659d9887c0f612ddd827d4c084ffdab840
kmranrg <kmranrg@gmail.com>
2020-09-10 16:15:21 +0530

    added list

---
+++ 8_List/main.py
@@ -0,0 +1,25 @@
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.list import MDList, ThreeLineListItem
+from kivy.uix.scrollview import ScrollView
+
+
+class DemoApp(MDApp):
+
+    def build(self):
+        screen = Screen()
+
+        scroll = ScrollView()
+        list_view = MDList()
+        scroll.add_widget(list_view)
+
+        for i in range(20):
+            items = ThreeLineListItem(text='Item ' + str(i+1), secondary_text='Hello world',
+                                      tertiary_text='third text')
+            list_view.add_widget(items)
+
+        screen.add_widget(scroll)
+        return screen
+
+
+DemoApp().run()
